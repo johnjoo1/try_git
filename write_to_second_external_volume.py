95682d23d8fcd19e9c7265a763d97184adc1f21a
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2020-10-28 12:47:12 -0700

    Pushed from Domino: https://wb44d2mo215888.workbench-analyze-team-sandbox.domino.tech/u/integration-test/demo-project/runs/5f99ca13bfd7c2094ed286e1

--- write_to_second_external_volume.py
+++ write_to_second_external_volume.py
@@ -5,5 +5,6 @@ src = "/mnt/datasets/my_dataset/latest/iris.csv".format(demo.project_name)
 dst = "/domino/nfs/{}/iris.csv".format(demo.edv2)
 
 shutil.copyfile(src, dst)
+print("Hello Demo!")
 
 print("copied {} to {}".format(src, dst))
