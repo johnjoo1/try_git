e818d9244b5dbd6ae2da8b30845e8d6a9a54b047
John Joo <johnjoo1@users.noreply.github.com>
2020-11-03 15:58:52 -0800

    Update main-gbp.py

--- main-gbp.py
+++ main-gbp.py
@@ -11,6 +11,7 @@ plt.switch_backend('Agg')
 
 # Print to console/stdout
 print("Hello, {0}!".format(os.environ['DOMINO_PROJECT_OWNER']))
+print("Current working directory:{cwd}".format(cwd=os.getcwd()))
 
 
 # Define a helper function to generate a random number
